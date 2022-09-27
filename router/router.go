f892161256075cb995f7976991b9bdb2e8c22cf0
topcoder520 <38901719+topcoder520@users.noreply.github.com>
2022-09-27 15:28:17 +0800

    add method ping()

--- router/router.go
+++ router/router.go
@@ -1,14 +1,23 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/topcoder520/rebotgoapi/mouse"
+	"github.com/topcoder520/rebotgoapi/util"
 )
 
 func GetRouter() *gin.Engine {
 	// Creates a gin router with default middleware:
 	// logger and recovery (crash-free) middleware
 	router := gin.Default()
+
+	router.Use(Cors()) //解决跨域
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, util.Success())
+	})
 
 	//鼠标操作
 	v1 := router.Group("/mouse")
@@ -36,4 +45,18 @@ func GetRouter() *gin.Engine {
 		v1.GET("/wheelscrolldown", mouse.MouseScrollMouseDown)
 	}
 	return router
+}
+func Cors() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		method := context.Request.Method
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		context.Header("Access-Control-Allow-Credentials", "true")
+		if method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+		}
+		context.Next()
+	}
 }
